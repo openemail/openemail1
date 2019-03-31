18b77bf16f9ada34adae739b758c0dbc1ddca11a
Chinthaka Deshapriya <chinthaka@cybergatelab.com>
2019-03-31 10:37:01 +0530

    New scripts add

--- generate_config.sh
+++ generate_config.sh
@@ -244,8 +244,8 @@ FUSIONDIRECTORY_ADMIN_USER=fdadmin
 FUSIONDIRECTORY_ADMIN_PASS=openemail
 ORGANIZATION=Openemail IO
 SLD=$(echo $(hostname -d) | cut -f1 -d .)
-TLD=$(echo $(hostname -d) | cut -f1 -d .)
-BASE_DN=dc=${SLD},dc=${TLD}
+TLD=$(echo $(hostname -d) | cut -f2 -d .)
+BASE_DN=dc=`echo${SLD}`,dc=`echo${TLD}`
 ENABLE_READONLY_USER=true
 READONLY_USER_USER=reader
 READONLY_USER_PASS=openemail
@@ -260,8 +260,8 @@ SSL_HELPER_PREFIX=ldap
 
 ENABLE_REPLICATION=false
 REPLICATION_CONFIG_SYNCPROV=binddn="cn=admin,cn=config" bindmethod=simple credentials="openemail" searchbase="cn=config" type=refreshAndPersist retry="60 +" timeout=1
-REPLICATION_DB_SYNCPROV=binddn="cn=admin,dc=${SLD},dc=${TLD}" bindmethod=simple credentials="admin" searchbase="dc=${SLD},dc=${TLD}" type=refreshAndPersist interval=00:00:00:10 retry="60 +" timeout=1
-REPLICATION_HOSTS=ldap://ldap1.${DOMAIN} ldap://ldap2.${DOMAIN}
+REPLICATION_DB_SYNCPROV=binddn="cn=admin,dc=`echo${SLD}`,dc=`echo${TLD}`" bindmethod=simple credentials="admin" searchbase="dc=`echo${SLD}`,dc=`echo${TLD}`" type=refreshAndPersist interval=00:00:00:10 retry="60 +" timeout=1
+REPLICATION_HOSTS=ldap://ldap1.$(echo${DOMAIN}) ldap://ldap2.$echo(${DOMAIN})
 REMOVE_CONFIG_AFTER_SETUP=false
 
 BACKUP_CONFIG_CRON_PERIOD=0 4 * * *
