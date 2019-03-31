b3f44cd2c7bc0a7290de2af498e5a98f5e78e496
Chinthaka Deshapriya <chinthaka@cybergatelab.com>
2019-03-31 12:44:51 +0530

    Modified

--- generate_config.sh
+++ generate_config.sh
@@ -78,7 +78,7 @@ elif [ ${MEM_TOTAL} -le "3670016" ]; then
   case $response in
     [nN][oO]|[nN])
       SKIP_SOLR=n
-      ;;
+      ;;export
     *)
       SKIP_SOLR=y
     ;;
@@ -244,9 +244,9 @@ CONFIG_PASS=openemail
 FUSIONDIRECTORY_ADMIN_USER=fdadmin
 FUSIONDIRECTORY_ADMIN_PASS=openemail
 ORGANIZATION="Openemail IO"
-export SLD=$(echo $(hostname -d) | cut -f1 -d .)
-export TLD=$(echo $(hostname -d) | cut -f2 -d .)
-export BASE_DN="dc=`echo ${SLD}`,dc=`echo ${TLD}`"
+SLD=$(echo $(hostname -d) | cut -f1 -d .)
+TLD=$(echo $(hostname -d) | cut -f2 -d .)
+ASE_DN=dc=`echo ${SLD}`,dc=`echo ${TLD}`
 ENABLE_READONLY_USER=true
 READONLY_USER_USER=reader
 READONLY_USER_PASS=openemail
@@ -262,13 +262,13 @@ SSL_HELPER_PREFIX=ldap
 ENABLE_REPLICATION=false
 REPLICATION_CONFIG_SYNCPROV=binddn="cn=admin,cn=config" bindmethod=simple credentials="openemail" searchbase="cn=config" type=refreshAndPersist retry="60 +" timeout=1
 REPLICATION_DB_SYNCPROV=binddn="cn=admin,${BASE_DN}" bindmethod=simple credentials="admin" searchbase="${BASE_DN}" type=refreshAndPersist interval=00:00:00:10 retry="60 +" timeout=1
-exporrt LDAP1="ldap1.${DOMAIN}"
-expot LDAP2="ldap2.${DOMAIN}"
-REPLICATION_HOSTS="ldap://${LDAP1} ldap://${LDAP2}"
+LDAP1=ldap1.${DOMAIN}
+LDAP2=ldap2.${DOMAIN}
+REPLICATION_HOSTS=$(echo "ldap://${LDAP1} ldap://${LDAP2}")
 REMOVE_CONFIG_AFTER_SETUP=false
 
-BACKUP_CONFIG_CRON_PERIOD="0 4 * * *"
-BACKUP_DATA_CRON_PERIOD="0 4 * * *"
+BACKUP_CONFIG_CRON_PERIOD='0 4 * * *'
+BACKUP_DATA_CRON_PERIOD='0 4 * * *'
 BACKUP_TTL=15
 
 ZABBIX_HOSTNAME=openldap-fusiondirectory
