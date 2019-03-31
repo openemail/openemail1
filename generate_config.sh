b9c90229ecadfba3b03871ddb1ab5a4e12bd3e68
Chinthaka Deshapriya <chinthaka@cybergatelab.com>
2019-03-31 13:56:47 +0530

    Modified

--- generate_config.sh
+++ generate_config.sh
@@ -243,10 +243,10 @@ ADMIN_PASS=openemail
 CONFIG_PASS=openemail
 FUSIONDIRECTORY_ADMIN_USER=fdadmin
 FUSIONDIRECTORY_ADMIN_PASS=openemail
-ORGANIZATION=Openemail\ IO
+OORGANIZATION=(Openemail\ IO)
 SLD=$(echo $(hostname -d) | cut -f1 -d .)
 TLD=$(echo $(hostname -d) | cut -f2 -d .)
-BASE_DN=dc=${SLD},dc=${TLD}
+BASE_DN=dc=$(echo ${SLD}),dc=$(echo ${TLD})
 ENABLE_READONLY_USER=true
 READONLY_USER_USER=reader
 READONLY_USER_PASS=openemail
@@ -259,17 +259,12 @@ TLS_CIPHER_SUITE=ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:
 TLS_VERIFY_CLIENT=never
 SSL_HELPER_PREFIX=ldap
 ENABLE_REPLICATION=false
-REPLICATION_CONFIG_SYNCPROV=binddn="cn=admin,cn=config"\ bindmethod=simple credentials="openemail" searchbase="cn=config"\ type=refreshAndPersist\ retry="60 +"\ timeout=1
-REPLICATION_DB_SYNCPROV=binddn="cn=admin,${BASE_DN}"\ bindmethod=simple credentials="admin"\ searchbase=${BASE_DN} type=refreshAndPersist\ interval=00:00:00:10 retry="60 +"\ timeout=1
-LDAP1=ldap1.${DOMAIN}
-LDAP2=ldap2.${DOMAIN}
-REPLICATION_HOSTS=ldap://${LDAP1}\ ldap://${LDAP2}
+REPLICATION_CONFIG_SYNCPROV=(binddn="cn=admin,cn=config"\ bindmethod=simple\ credentials="openemail"\ searchbase="cn=config"\ type=refreshAndPersist\ retry="60 +"\ timeout=1)
+REPLICATION_DB_SYNCPROV=(binddn="cn=admin,${BASE_DN}"\ bindmethod=simple\ credentials="admin"\ searchbase=${BASE_DN}\ type=refreshAndPersist\ interval=00:00:00:10\ retry="60 +"\ timeout=1)
+LDAP1=ldap1.$DOMAIN
+LDAP2=ldap2.$DOMAIN
+REPLICATION_HOSTS=(ldap://$LDAP1\ ldap://$LDAP2)
 REMOVE_CONFIG_AFTER_SETUP=false
-
-BACKUP_CONFIG_CRON_PERIOD=0\ 4\ *\ *\ *
-BACKUP_DATA_CRON_PERIOD=0\ 4\ *\ *\ *
-BACKUP_TTL=15
-
 ZABBIX_HOSTNAME=openldap-fusiondirectory
 
 EOF
